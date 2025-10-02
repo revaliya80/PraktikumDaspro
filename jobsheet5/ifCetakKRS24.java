52375dad75ad00ac3e463875815d65460833e4e7
Refaliya-Vinandha <revaliya80@gmail.com>
2025-10-01 23:35:46 -0700

    Modifikasi Percobaan 1

--- jobsheet5/ifCetakKRS24.java
+++ jobsheet5/ifCetakKRS24.java
@@ -10,5 +10,8 @@ public class ifCetakKRS24{
         System.out.println("Pembayaran UKT terverifikasi");
         System.out.println("Silakan cetak KRS dan minta tanda tangan DPA");
     }
+    else {
+        System.out.println("Registrasi ditolak. Silahkan lunasi UKT terlebih Dahulu");
+    }
     }
 }
